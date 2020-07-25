9a0836d284db422688dd171d38695708be6c3b89
chris <44552743+return5@users.noreply.github.com>
2020-07-25 15:52:12 -0500

    Update blinkled.ino

--- blinkled.ino
+++ blinkled.ino
@@ -1,3 +1,8 @@
+//code for part 1 of my arduino tutorial: https://lacklusterstudios.blogspot.com/2020/07/arduino-introduction.html
+//Blinking LED.  Blinks a LED on and off.
+//licensed under GPL 3.0
+//Author: chris nichols (github/rteturn5)
+
 #define LEDPIN 5   //we will use pin 5 on the arduino for this project
 
 void setup() { 
